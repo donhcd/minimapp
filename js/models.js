46faa0e767bb4317146428d13c4ff594634a21f6
Donald Huang <don.hcd@gmail.com>
2012-10-28 09:30:49 -0400

    adjusted intentation in models.js

--- js/models.js
+++ js/models.js
@@ -1,4 +1,3 @@
- (function () {
 //    // TODO(jzzhang) find a place for this
 //    var infoBubble = new InfoBubble({
 //        map: gmap,
@@ -22,190 +21,189 @@
 //    // binds click event on infobubble to render infowindow
 
 
-    window.markers = {};
+window.markers = {};
 
-    window.Entity = Parse.Object.extend("Entity", {
-        // This should be constructed with a map providing the name, ownerId,
-        // layerid, and latLng coordinates
-        initialize: function() {
+window.Entity = Parse.Object.extend("Entity", {
+    // This should be constructed with a map providing the name, ownerId,
+    // layerid, and latLng coordinates
+    initialize: function() {
 
-            if (!(this.get("name") &&
-                    this.get("ownerId") && this.get("layerid") &&
-                    this.get("lat") && this.get("lng"))) {
-                if (this.get("lat")) {
-                    // We weren't given a user. This should never happen.
-                    throw "something is fucked up";
-                }
-                // everything is probably missing
-//                console.log(this.attributes.name +', '+
-//                    this.attributes.ownerId +', '+ this.attributes.layerid +', '+
-//                    this.attributes.lat +', '+ this.attributes.lng);
-//                console.log(this.get("name") +', '+
-//                    this.get("ownerId") +', '+ this.get("layerid") +', '+
-//                    this.get("lat") +', '+ this.get("lng"));
-//                console.log(this);
-                return;
-//                throw "name, ownerId, layerid, lat, and lng must be " +
-//                      "provided to the constructor for an Entity";
+        if (!(this.get("name") &&
+              this.get("ownerId") && this.get("layerid") &&
+                  this.get("lat") && this.get("lng"))) {
+            if (this.get("lat")) {
+                // We weren't given a user. This should never happen.
+                throw "something is fucked up";
             }
-            this.save();
+            // everything is probably missing
+            // console.log(this.attributes.name +', '+
+            //     this.attributes.ownerId +', '+ this.attributes.layerid +', '+
+            //     this.attributes.lat +', '+ this.attributes.lng);
+            // console.log(this.get("name") +', '+
+            //     this.get("ownerId") +', '+ this.get("layerid") +', '+
+            //     this.get("lat") +', '+ this.get("lng"));
+            // console.log(this);
+            // throw "name, ownerId, layerid, lat, and lng must be " +
+            //       "provided to the constructor for an Entity";
+            return;
         }
-    });
+        this.save();
+    }
+});
 
-    /* expect a layerid field */
-    window.EntitySet = Parse.Collection.extend({
+/* expect a layerid field */
+window.EntitySet = Parse.Collection.extend({
 
-        model: Entity,
+    model: Entity,
 
-        addEntity: function(entity) {
-            this.add(entity);
-            entity.initialize();
+    addEntity: function(entity) {
+        this.add(entity);
+        entity.initialize();
+    }
+});
+
+window.Layer = Parse.Object.extend("Layer", {
+
+    initialize: function() {
+        if (!this.get("layerid")) {
+            throw "Layer initializer requires layerid attribute";
         }
-    });
+        this.entities = new EntitySet();
+        this.entities.query = new Parse.Query(Entity);
+        this.entities.query.equalTo("layerid", this.get("layerid"));
+    },
 
-    window.Layer = Parse.Object.extend("Layer", {
-
-        initialize: function() {
-            if (!this.get("layerid")) {
-                throw "Layer initializer requires layerid attribute";
-            }
-            this.entities = new EntitySet();
-            this.entities.query = new Parse.Query(Entity);
-            this.entities.query.equalTo("layerid", this.get("layerid"));
-        },
-
-        getImage: function() {
-            switch(this.get("layerid")) {
-                case 'users':
-                    return 'images/markers/man.png';
-                    //return 'scripts/images/person_generic.png';
-                case 'landmarks':
-                    return 'images/markers/landmark.png';
-                    //return 'scripts/images/green_75.png';
-            }
-        },
-
-        addEntity: function(entity) {
-            entity.set('layerid', this.get('layerid'));
-            entity.set('image', this.getImage());
-            this.entities.addEntity(entity);
+    getImage: function() {
+        switch(this.get("layerid")) {
+            case 'users':
+                return 'images/markers/man.png';
+            //return 'scripts/images/person_generic.png';
+            case 'landmarks':
+                return 'images/markers/landmark.png';
+            //return 'scripts/images/green_75.png';
         }
-    });
+    },
 
-    window.Layers = Parse.Collection.extend({
+    addEntity: function(entity) {
+        entity.set('layerid', this.get('layerid'));
+        entity.set('image', this.getImage());
+        this.entities.addEntity(entity);
+    }
+});
 
-        model: Layer,
+window.Layers = Parse.Collection.extend({
 
-        initialize: function() {
-            _.bindAll(this, 'addEntity');
-            this.add(new Layer({
-                layerid: 'users',
-                layerName: 'People',
-                layerNameSingular: 'Person'
-            }));
-            this.add(new Layer({
-                layerid: 'landmarks',
-                layerName: 'Landmarks',
-                layerNameSingular: 'Landmark'
-            }));
-            this.each(function(layer) {
-                layer.entities.fetch({
-                    success: function(entities) {
-                       entities.each(function(entity) {
-                           entity.initialize();
-                       });
-                    },
-                    error: function(entities) {
-                        //alert("fuck, got an error");
-                    }
-                });
-            });
-        },
+    model: Layer,
 
-        getLayerWithName: function(name) {
-            return this.find(function(layer) {
-                return layer.get("layerNameSingular") === name;
-            });
-        },
-
-        addEntity: function(entity) {
-            var layerToAddEntityTo =
-                this.getLayerWithName(entity.get("layerNameSingular"));
-            if (layerToAddEntityTo) {
-                layerToAddEntityTo.addEntity(entity);
-            } else {
-                alert("this layer is not a thing");
-            }
-        }
-    });
-
-    var layers = {};
-
-    window.Map = Parse.Object.extend("Map", {
-        defaults: {
-            // weren't shown before but now are
-            layersNowShown: [],
-            // were shown before but now should hide
-            layersNowHidden: []
-        },
-        initialize: function() {
-            this.layers = new Layers();
-//            this.InstantiateWithIds(this.get("subscribed"));
-//            setInterval(function () {
-//                if (Parse.User.current()) {
-//                    // callback handles each layer update separately!
-//                    _.each(this.get("subscribed"), this.UpdateLocalLayer, this);
-//                }
-//            }.bind(this), 30000);
-        },
-        addEntity: function(entity) {
-            // (assume ownerId is already authenticated)
-
-            // only if it valid:
-            console.log("adding entity to layer: " + entity.get("layerNameSingular"));
-
-            this.layers.addEntity(entity);
-        },
-        DoWithEntities: function(layerid, action) {
-            if (layers[layerid]) {
-                // if we already have a cached version, use that
-                action(layers[layerid].entities.models);
-                return;
-            } else {
-                console.log("Called DoWithEntities with no" +
-                        "such local layer: " + layerid);
-                console.log("local layers:");
-                console.log(layers);
-            }
-        },
-        UpdateLocalLayer: function(layerid) {
-            layers[layerid].entities.fetch({
+    initialize: function() {
+        _.bindAll(this, 'addEntity');
+        this.add(new Layer({
+            layerid: 'users',
+            layerName: 'People',
+            layerNameSingular: 'Person'
+        }));
+        this.add(new Layer({
+            layerid: 'landmarks',
+            layerName: 'Landmarks',
+            layerNameSingular: 'Landmark'
+        }));
+        this.each(function(layer) {
+            layer.entities.fetch({
                 success: function(entities) {
-                   entities.each(function(entity) {
-                       entity.initialize();
-                   });
+                    entities.each(function(entity) {
+                        entity.initialize();
+                    });
                 },
                 error: function(entities) {
                     //alert("fuck, got an error");
                 }
             });
-        },
-        Subscribe: function(layerids) {
-            this.subscribed = _.union(layerids, this.subscribed);
-        },
-        Unsubscribe: function(layerids) {
-            this.subscribed = _.difference(this.subscribed, layerids);
-        },
-        InstantiateWithIds: function(ids) {
-            _.each(
-                    ids,
-                    function(id) {
-                        var layer = new Layer({layerid: id});
-                        layers[id] = layer;
-                        this.UpdateLocalLayer(id);
-                    },
-                    this);
-        }
-    });
+        });
+    },
 
- })();
+    getLayerWithName: function(name) {
+        return this.find(function(layer) {
+            return layer.get("layerNameSingular") === name;
+        });
+    },
+
+    addEntity: function(entity) {
+        var layerToAddEntityTo =
+            this.getLayerWithName(entity.get("layerNameSingular"));
+        if (layerToAddEntityTo) {
+            layerToAddEntityTo.addEntity(entity);
+        } else {
+            alert("this layer is not a thing");
+        }
+    }
+});
+
+var layers = {};
+
+window.Map = Parse.Object.extend("Map", {
+    defaults: {
+        // weren't shown before but now are
+        layersNowShown: [],
+        // were shown before but now should hide
+        layersNowHidden: []
+    },
+    initialize: function() {
+        this.layers = new Layers();
+        // this.InstantiateWithIds(this.get("subscribed"));
+        // setInterval(function () {
+        //     if (Parse.User.current()) {
+        //         // callback handles each layer update separately!
+        //         _.each(this.get("subscribed"), this.UpdateLocalLayer, this);
+        //     }
+        // }.bind(this), 30000);
+    },
+    addEntity: function(entity) {
+        // (assume ownerId is already authenticated)
+
+        // only if it valid:
+        console.log("adding entity to layer: " +
+                    entity.get("layerNameSingular"));
+
+        this.layers.addEntity(entity);
+    },
+    DoWithEntities: function(layerid, action) {
+        if (layers[layerid]) {
+            // if we already have a cached version, use that
+            action(layers[layerid].entities.models);
+            return;
+        } else {
+            console.log("Called DoWithEntities with no" +
+                        "such local layer: " + layerid);
+            console.log("local layers:");
+            console.log(layers);
+        }
+    },
+    UpdateLocalLayer: function(layerid) {
+        layers[layerid].entities.fetch({
+            success: function(entities) {
+                entities.each(function(entity) {
+                    entity.initialize();
+                });
+            },
+            error: function(entities) {
+                //alert("fuck, got an error");
+            }
+        });
+    },
+    Subscribe: function(layerids) {
+        this.subscribed = _.union(layerids, this.subscribed);
+    },
+    Unsubscribe: function(layerids) {
+        this.subscribed = _.difference(this.subscribed, layerids);
+    },
+    InstantiateWithIds: function(ids) {
+        _.each(
+            ids,
+            function(id) {
+                var layer = new Layer({layerid: id});
+                layers[id] = layer;
+                this.UpdateLocalLayer(id);
+            },
+            this);
+    }
+});
