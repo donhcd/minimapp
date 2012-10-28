fce215e08075d36935182c56c85aa4df52b7bad1
Donald Huang <don.hcd@gmail.com>
2012-10-28 08:40:49 -0400

    listened for add and reset in layers, so now entities show on load

--- js/main.js
+++ js/main.js
@@ -181,20 +181,24 @@ window.LayerView = Backbone.View.extend({
 
     initialize: function() {
         this.collection = this.model.entities;
-        this.collection.bind('add', this.render, this);
+        _.bindAll(this, 'render', 'drawEntity');
+        this.collection.bind('add', this.drawEntity);
+        this.collection.bind('reset', this.render);
     },
 
     render: function() {
         this.entityViews = {};
-        this.collection.each(function(entity) {
-            var entityView = new EntityView({
-                model: entity,
-                gmap: this.options.gmap,
-                image: this.model.getImage()
-            });
-            entityView.render();
-            this.entityViews[entity.get('name')] = entityView;
-        }, this);
+        this.collection.each(this.drawEntity);
+    },
+
+    drawEntity: function(entity) {
+        var entityView = new EntityView({
+            model: entity,
+            gmap: this.options.gmap,
+            image: this.model.getImage()
+        });
+        entityView.render();
+        this.entityViews[entity.get('name')] = entityView;
     }
 
 });
