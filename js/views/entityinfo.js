81b267637a627b0cf4c26fccc783d1ff4054357c
Jim Zhang <zjimzhangz@gmail.com>
2012-11-01 02:51:15 -0400

    fixed tabs and centers

--- js/views/entityinfo.js
+++ js/views/entityinfo.js
@@ -41,36 +41,3 @@ define([
 
     return EntityInfoView;
 });
-
-/*
-render: function(entity) {
-            console.log("rendering infowindow");
-            console.log(entity.marker.title);
-            console.log(entity.marker.icon);
-            console.log(entity.get("lat"));
-            console.log(entity.get("lng"));
-            console.log(entity.get("ownerId"));
-            posStr = entity.get("lat") + "," + entity.get("lng") ;
-            markerStr = "&markers=color:blue%7Clabel:S%7C" + posStr;
-            var static_map = "http://maps.googleapis.com/maps/api/staticmap?"+
-                            "center="+posStr+
-                            "&zoom=13&size=450x120&sensor=false"+markerStr;
-            console.log(static_map);
-            //markerOwner : entity.get("ownerId").getUsername()
-            var variables = {
-                markerTitle: entity.marker.title, 
-                markerOwner : entity.get("ownerId").id ,
-                markerMap: static_map,
-                Description: entity.get("description")
-            };
-            console.log(variables);
-            //var variables = {};
-            var template = _.template($("#info-template").html(), {
-                markerTitle: entity.marker.title, 
-                markerOwner : entity.get("ownerId").id ,
-                markerMap: static_map,
-                Description: entity.get("description")
-            });
-            $(this.el).html(template);
-        }
-*/
