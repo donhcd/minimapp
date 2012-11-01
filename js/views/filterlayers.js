1cac0f736dfa9ab23f88112f9b7eb07e89713e22
Tom Xu <tzx@andrew.cmu.edu>
2012-11-01 03:19:37 -0400

    Filtering (Select all/none) changes

--- js/views/filterlayers.js
+++ js/views/filterlayers.js
@@ -16,25 +16,34 @@ define([
             "click #select-none-button": "selectNone"
         },
 
-        render: function() {
+        render: function () {
             this.delegateEvents();
             this.$el.html(this.template({
                 layers: this.collection.toJSON()
             }));
             console.log('rendered layers view');
+//          Attempt at a view that remembers previously checked values.
+//          Doesn't seem to be working at this time.
+//          this.collection.each(function (layer) {
+//              console.log('collection name:' + layer.get('layerName'));
+//              $('#' + layer.get('layerName')).attr('checked', $('#' + layer.get('layerName')).checked);
+//              $('#' + layer.get('layerName')).checkboxradio('refresh');
+//          });
         },
 
         selectAll: function(event) {
             this.collection.each(function(layer) {
-                // TODO(donaldh) show this in the UI
+                console.log('collection name:' + layer.get('layerName'));
                 $('#' + layer.get('layerName'))[0].checked = 1;
+                $('#' + layer.get('layerName')).checkboxradio("refresh");
             });
+
         },
 
         selectNone: function(event) {
             this.collection.each(function(layer) {
-                // TODO(donaldh) show this in the UI
                 $('#' + layer.get('layerName'))[0].checked = 0;
+                $('#' + layer.get('layerName')).checkboxradio("refresh");
             });
         },
 
