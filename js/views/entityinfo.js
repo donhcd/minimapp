f56a6aaa17624338bb4648e8e4710ebaea883224
Jim Zhang <zjimzhangz@gmail.com>
2012-11-01 02:06:32 -0400

    Modified info window
    Implemented more legitimate layout. also added subscribe entity button that connects to view

--- js/views/entityinfo.js
+++ js/views/entityinfo.js
@@ -13,9 +13,29 @@ define([
                 this.collection.remove(entity);
             }, this);
         },
+        events: {
+            "click .subscribe_entity": "subscribeEntity"
+        },
+        subscribeEntity: function(e){
+            console.log("subscribe entity clicked");
+        },       
 
         render: function() {
-            this.$el.html(this.template(this.model.toJSON()));
+            var modelVariables = this.model.toJSON();
+            
+            var posStr = this.model.get("lat") + "," + this.model.get("lng") ;
+            var markerStr = "&markers=color:blue%7Clabel:S%7C" + posStr;
+            var static_map = "http://maps.googleapis.com/maps/api/staticmap?"+
+                            "center="+posStr+
+                            "&zoom=13&size=450x120&sensor=false"+markerStr;
+            $.extend(modelVariables,{markerMapUri: static_map});
+            
+            
+            this.$el.html(this.template(modelVariables));
+        
+        
+        
+        
         }
     });
 
