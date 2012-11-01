b907c3117c1020f51a9ba165700332a51d844ec4
Donald Huang <don.hcd@gmail.com>
2012-11-01 03:03:58 -0400

    fixed some style

--- js/views/entityinfo.js
+++ js/views/entityinfo.js
@@ -13,29 +13,26 @@ define([
                 this.collection.remove(entity);
             }, this);
         },
+
         events: {
-            "click .subscribe_entity": "subscribeEntity"
+            'click .subscribe_entity': 'subscribeEntity'
         },
+
         subscribeEntity: function(e){
-            console.log("subscribe entity clicked");
-        },       
+            console.log('subscribe entity clicked');
+        },
 
         render: function() {
             var modelVariables = this.model.toJSON();
-            
-            var posStr = this.model.get("lat") + "," + this.model.get("lng") ;
-            var markerStr = "&markers=color:blue%7Clabel:S%7C" + posStr;
-            var static_map = "http://maps.googleapis.com/maps/api/staticmap?"+
-                            "center="+posStr+
-                            "&zoom=13&size=450x120&sensor=false"+markerStr;
-            $.extend(modelVariables,{markerMapUri: static_map});
-            
-            
+
+            var posStr = this.model.get('lat') + ',' + this.model.get('lng') ;
+            var markerStr = '&markers=color:blue%7Clabel:S%7C' + posStr;
+            var staticMap = 'http://maps.googleapis.com/maps/api/staticmap?' +
+                            'center=' + posStr +
+                            '&zoom=13&size=450x120&sensor=false' + markerStr;
+            $.extend(modelVariables, {markerMapUri: staticMap});
+
             this.$el.html(this.template(modelVariables));
-        
-        
-        
-        
         }
     });
 
